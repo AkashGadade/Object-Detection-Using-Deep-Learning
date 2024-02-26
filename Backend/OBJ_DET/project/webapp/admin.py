7a379708cc9501d5d95086376118bf91dcf523fa
AkashGadade <gadadeakash321@gmail.com>
2024-02-27 01:26:17 +0530

    I have added the functionality for uplaoding the image , it will upload image to media folder and and add its url in sqlite3 database

--- Backend/OBJ_DET/project/webapp/admin.py
+++ Backend/OBJ_DET/project/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.admin.sites import site
+from .models import ImageData
 # Register your models here.
+class ImageDataAdmin(admin.ModelAdmin):
+    list_display = ('id','img')
+admin.site.register(ImageData,ImageDataAdmin)
--- Backend/OBJ_DET/project/webapp/models.py
+++ Backend/OBJ_DET/project/webapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ImageData(models.Model):
+    imgname = models.CharField(max_length=100,null=True)
+    img  = models.ImageField(upload_to='images/',max_length=300,null=True)
+    
